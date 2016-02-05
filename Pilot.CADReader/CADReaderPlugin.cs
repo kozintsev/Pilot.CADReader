4d5f7c8074021ef1df2355dfdf0ca5976be3a118
kozintsev <o.kozintsev@gmail.com>
2016-02-05 17:24:36 +0300

    Add Pilot SDK

--- Pilot.CADReader/CADReaderPlugin.cs
+++ Pilot.CADReader/CADReaderPlugin.cs
@@ -1,12 +1,65 @@
-﻿using System;
+﻿using Ascon.Pilot.SDK;
 using System.Collections.Generic;
+using System.ComponentModel.Composition;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Windows;
 
 namespace Pilot.CADReader
 {
-    public class CADReaderPlugin
+    [Export(typeof(IObjectContextMenu))]
+    public class CADReaderPlugin : IObjectContextMenu
     {
+        private const string MenuItemName = "PluginMenuItem";
+        private const string DocumentMenuItemName = "DocumentPluginMenuItem";
+        private const string ContextMenuItemName = "ContextPluginMenuItem";
+        private const string ManySelectedMenuItemName = "ManySelectedMenuItemName";
+
+        public void BuildContextMenu(IMenuHost menuHost, IEnumerable<IDataObject> selection, bool isContext)
+        {
+            var objects = selection.ToList();
+            var icon = IconLoader.GetIcon("/Resources/menu_icon.svg");
+
+            var itemNames = menuHost.GetItems().ToList();
+            const string indexItemName = "miShowSharingSettings";
+            var insertIndex = itemNames.IndexOf(indexItemName) + 1;
+
+            // Show menu for the empty area within a folder
+            if (isContext)
+            {
+                menuHost.AddItem(ContextMenuItemName, "Menu for empty area", icon, insertIndex);
+                return;
+            }
+
+            // Show menu when a document is selected
+            if (objects.Count() == 1)
+            {
+                var current = objects.Single();
+                if (current.Type.HasFiles)
+                    menuHost.AddItem(DocumentMenuItemName, "Menu for document", icon, insertIndex);
+                else
+                    menuHost.AddItem(MenuItemName, "Menu for object", icon, insertIndex);
+            }
+
+            // Show menu for many selected objects
+            if (objects.Count > 1)
+            {
+                menuHost.AddItem(ManySelectedMenuItemName, "Menu for many objects", icon, insertIndex);
+            }
+        }
+
+        public void OnMenuItemClick(string itemName)
+        {
+            if (MenuItemName == itemName)
+                MessageBox.Show(string.Format("{0} was clicked", MenuItemName));
+
+            if (DocumentMenuItemName == itemName)
+                MessageBox.Show(string.Format("{0} was clicked", DocumentMenuItemName));
+
+            if (ContextMenuItemName == itemName)
+                MessageBox.Show(string.Format("{0} was clicked", ContextMenuItemName));
+
+            if (ManySelectedMenuItemName == itemName)
+                MessageBox.Show(string.Format("{0} was clicked", ManySelectedMenuItemName));
+        }
     }
 }
---
+++ Pilot.CADReader/IconLoader.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using System.Windows;
+
+namespace Pilot.CADReader
+{
+    class IconLoader
+    {
+        public static byte[] GetIcon(string relativePath)
+        {
+            var assembly = Assembly.GetEntryAssembly() ?? Assembly.GetExecutingAssembly();
+            Application.ResourceAssembly = assembly;
+            var uri = new Uri(relativePath, UriKind.Relative);
+            var info = Application.GetResourceStream(uri);
+            using (var memoryStream = new MemoryStream())
+            {
+                if (info == null)
+                    return null;
+
+                info.Stream.CopyTo(memoryStream);
+                return memoryStream.ToArray();
+            }
+        }
+    }
+}
