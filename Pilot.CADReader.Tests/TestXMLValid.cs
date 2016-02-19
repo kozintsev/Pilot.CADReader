57d8593ab9e24ff8b685d15a270515136cefdc1e
kozintsev <o.kozintsev@gmail.com>
2016-02-19 13:09:42 +0300

    Add unit test

--- Pilot.CADReader.Tests/TestXMLValid.cs
+++ Pilot.CADReader.Tests/TestXMLValid.cs
@@ -4,16 +4,30 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Xml;
+using System.Xml.Linq;
+using System.Diagnostics;
 
 namespace Pilot.CADReader.Tests
 {
     [TestClass]
+    [DeploymentItem(@"TestSourceFiles\")]
     public class TestXMLValid
     {
         [TestMethod]
-        public void XMLValidTest()
+        public void LoadFromFileTest()
         {
-
+            try
+            {
+                XmlReader reader = XmlReader.Create("MetaInfo.xml");
+                XDocument xDoc = XDocument.Load(reader);
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine("SpwAnalyzer threw exception: " + ex.Message);
+            }
         }
+
+
     }
 }
