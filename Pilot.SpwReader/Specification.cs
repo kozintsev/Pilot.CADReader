75d08efbfa71cb6c9813b95faaf31233ab5071f2
Oleg Kozintsev <o.kozintsev@gmail.com>
2016-04-29 16:08:26 +0400

    research

--- Pilot.SpwReader/Specification.cs
+++ Pilot.SpwReader/Specification.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.Collections.Generic;
 
 namespace Ascon.Pilot.SDK.SpwReader
 {
@@ -10,6 +7,8 @@ namespace Ascon.Pilot.SDK.SpwReader
         public string Name { get; set; }
         public string Designation { get; set; }
         public string CurrentPath { get; set; }
+        public Specification Children { get; set; }
+        public Specification Parent { get; set; }
 
         public List<SpcObject> ListSpcObjects { get; set; }
 
