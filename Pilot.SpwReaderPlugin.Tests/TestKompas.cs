cc246378d229c1b21337fe60f1de6ece53282d64
Oleg Kozintsev <o.kozintsev@gmail.com>
2017-12-07 12:19:37 +0400

    add test filse

--- Pilot.SpwReaderPlugin.Tests/TestKompas.cs
+++ Pilot.SpwReaderPlugin.Tests/TestKompas.cs
@@ -12,21 +12,40 @@ namespace Pilot.SpwReaderPlugin.Tests
         public string StartupPath = Directory.GetParent(@"./").FullName;
         public const string PilotPrinterFolder = @"C:\ProgramData\ASCON\Pilot_Print";
 
+        public bool IsTmpFile()
+        {
+            const string tmpXps = @"C:\ProgramData\ASCON\Pilot_Print\tmp.xps";
+            if (!File.Exists(tmpXps))
+                return false;
+            var length = new FileInfo(tmpXps).Length;
+            return length > 0;
+        }
+
         [TestMethod]
-        public void TestPrintXpsFile()
+        public void TestPrintSpwToXpsFile()
         {
             using (var kompas = new KomapsShell())
             {
-                const string path = @"\Spc.spw";
+                const string path = @"\078.505.9.0100.00.SPW";
                 kompas.InitKompas(out var result);
                 kompas.PrintToXps(StartupPath + path);
                 Assert.IsTrue(string.IsNullOrEmpty(result));
             }
+            var isFile = false;
             var files = Directory.GetFiles(PilotPrinterFolder);
-            var isFile = files.Any(file => file.Contains(".xps"));
+            foreach (var file in files)
+            {
+                if (file.Contains(".xps"))
+                {
+                    var length = new FileInfo(file).Length;
+                    if (length > 0)
+                    {
+                        isFile = true;
+                        break;
+                    }
+                }
+            }
             Assert.IsTrue(isFile, "Tmp xps file not found");
-            //var length = new FileInfo(tempXps).Length;
-            //Assert.IsTrue(length > 0, "Error empty file");
         }
 
         [TestMethod]
