2c7b7106cc0b80ef56fd1b72047364fa9851585b
kozintsev <o.kozintsev@gmail.com>
2017-12-17 15:18:11 +0400

    refactoring md5 checker

--- Pilot.CadReaderPlugin/CalculatorMd5Checksum.cs
+++ Pilot.CadReaderPlugin/CalculatorMd5Checksum.cs
@@ -4,7 +4,7 @@ using System.Security.Cryptography;
 
 namespace Ascon.Pilot.SDK.CadReader
 {
-    static class CalculatorMd5Checksum
+    public static class CalculatorMd5Checksum
     {
         public static string Go(string path)
         {
@@ -14,22 +14,22 @@ namespace Ascon.Pilot.SDK.CadReader
             {
                 if (!File.Exists(path))
                     return null;
-                var fs = File.OpenRead(path);
+                using (var fs = File.OpenRead(path))
+                using (var md5 = new MD5CryptoServiceProvider())
                 {
-                    MD5 md5 = new MD5CryptoServiceProvider();
                     var fileData = new byte[fs.Length];
                     fs.Read(fileData, 0, (int)fs.Length);
                     var checkSum = md5.ComputeHash(fileData);
                     var result = BitConverter.ToString(checkSum).Replace("-", string.Empty).ToLower();
                     return result;
                 }
+
             }
             catch (Exception)
             {
-
                 return null;
             }
-            
+
         }
     }
 }
