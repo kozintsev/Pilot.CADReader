71f2febf0f53e741ccc51b9eae68bb2493af15dd
Oleg Kozintsev <o.kozintsev@gmail.com>
2017-12-28 16:27:43 +0400

    cleaning is not necessary

--- KompasAttrAutoImport/KompasAttrAutoImport.cs
+++ KompasAttrAutoImport/KompasAttrAutoImport.cs
@@ -1,13 +1,10 @@
-﻿using System;
-using System.IO;
-using System.Linq;
-using System.Windows;
+﻿using System.IO;
 using System.ComponentModel.Composition;
 using System.Threading;
 using System.Threading.Tasks;
 using Ascon.Pilot.SDK.ObjectCard;
 using KompasFileReader.Analyzer;
-using KompasFileReader.Spc;
+using KompasFileReader.Model;
 
 // ReSharper disable InconsistentNaming
 
@@ -17,19 +14,9 @@ namespace Ascon.Pilot.SDK.KompasAttrAutoImport
     [Export(typeof(IObjectCardHandler))]
     public class KompasAttrAutoImport : IAutoimportHandler, IObjectCardHandler
     {
-
-        private readonly IObjectModifier _modifier;
-        private readonly IPilotDialogService _dialogService;
+        private IGeneralDocEntity _doc;
         private const string SOURCE_DOC_EXT = ".cdw";
         private const string SPW_EXT = "*.spw";
-
-        [ImportingConstructor]
-        public KompasAttrAutoImport(IObjectModifier modifier, IPilotDialogService dialogService)
-        {
-            _modifier = modifier;
-            _dialogService = dialogService;
-        }
-
 
         public bool Handle(string filePath, string sourceFilePath, AutoimportSource autoimportSource)
         {
@@ -53,6 +40,7 @@ namespace Ascon.Pilot.SDK.KompasAttrAutoImport
                     {
                         var spc = taskOpenSpwFile.Result.GetSpecification;
                         spc.FileName = sourceFilePath;
+                        _doc = spc;
                     }
                 }
 
@@ -64,9 +52,9 @@ namespace Ascon.Pilot.SDK.KompasAttrAutoImport
                     if (taskOpenCdwFile.Result.IsCompleted)
                     {
                         var drawing = taskOpenCdwFile.Result.Drawing;
+                        _doc = drawing;
                     }
                 }
-
             }
             Thread.Sleep(2000);
             return false;
@@ -78,60 +66,18 @@ namespace Ascon.Pilot.SDK.KompasAttrAutoImport
             var isObjectModification = context.EditiedObject != null;
             if (isObjectModification || context.IsReadOnly)
                 return false;
-
-            var parent = context.Parent;
-            var sourceAttr = context.DisplayAttributes.FirstOrDefault(a => a.Type == AttributeType.String);
-            if (sourceAttr == null)
+            if (_doc == null)
                 return false;
-
-            var sourceValue = parent.Attributes.FirstOrDefault(a => a.Key == sourceAttr.Name);
-            if (sourceValue.Value == null)
-                return false;
-
-            var targetAttr = context.DisplayAttributes.FirstOrDefault(a => a.Type == AttributeType.String);
-            if (targetAttr == null)
-                return false;
-
-            var valueToSet =
-                $"Parent is {sourceValue.Value}; IsDocument:{context.Parent.Type.HasFiles}; Can be mount:{context.Parent.Type.IsMountable}";
-
-            modifier.SetValue(targetAttr.Name, valueToSet);
+            var des = _doc.GetDesignation();
+            var name = _doc.GetName();
+            modifier.SetValue("mark", ValueTextClear(des));
+            modifier.SetValue("name", name);
             return true;
         }
 
         public bool OnValueChanged(IAttribute sender, AttributeValueChangedEventArgs args, IAttributeModifier modifier)
         {
-            var currentAttributeValues = string.Empty;
-            foreach (var displayAttribute in args.Context.DisplayAttributes)
-            {
-                currentAttributeValues += displayAttribute.Name == sender.Name
-                    ? args.NewValue
-                    : displayAttribute.Name + ": " + args.Context.AttributesValues[displayAttribute.Name] + Environment.NewLine;
-            }
-
-            if (args.Context.Type.Name == "Document" && sender.Name == "Sheet_number")
-            {
-                var newNameAttrValue = "Sheet no " + args.NewValue + "; " + (args.Context.EditiedObject == null ? " New object " : " Existed object");
-                modifier.SetValue("Name", newNameAttrValue);
-                return true;
-            }
-
             return false;
-        }
-
-        private static string Localize(AutoimportSource autoimportSource)
-        {
-            switch (autoimportSource)
-            {
-                case AutoimportSource.Unknown:
-                    return "Unknown";
-                case AutoimportSource.PilotXps:
-                    return "Pilot XPS printer";
-                case AutoimportSource.UserFolder:
-                    return "user auto-import directory";
-                default:
-                    throw new NotSupportedException();
-            }
         }
 
         private static bool IsFileExtension(string name, string ext)
@@ -143,27 +89,13 @@ namespace Ascon.Pilot.SDK.KompasAttrAutoImport
         }
 
         /// <summary>
-        /// Метод получает информацию из спецификации и возвращает экземпляр класса Specification
+        /// Очистка строки полученной из спецификации от служибных символов: $| и @/
         /// </summary>
-        /// <param name="filename">Имя файла</param>
-        /// <returns>Объект спецификации</returns>
-        private static Specification GetInformationFromKompas(string filename)
+        /// <param name="str">Строка которую нужно очистить</param>
+        /// <returns>Очищенная строка</returns>
+        private static string ValueTextClear(string str)
         {
-            using (var inputStream = new FileStream(filename, FileMode.Open, FileAccess.Read))
-            {
-                var ms = new MemoryStream();
-                inputStream.Seek(0, SeekOrigin.Begin);
-                inputStream.CopyTo(ms);
-                ms.Position = 0;
-                var taskOpenSpwFile = new Task<SpwAnalyzer>(() => new SpwAnalyzer(ms));
-                taskOpenSpwFile.Start();
-                taskOpenSpwFile.Wait();
-                if (!taskOpenSpwFile.Result.IsCompleted)
-                    return null;
-                var spc = taskOpenSpwFile.Result.GetSpecification;
-                spc.FileName = filename;
-                return spc;
-            }
+            return str?.Replace("$|", "").Replace(" @/", " ").Replace("@/", " ");
         }
     }
 }
